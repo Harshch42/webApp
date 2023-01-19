e9aa386ccb525ea37ab010799c4bf851e504314a
Harshch42 <115652647+Harshch42@users.noreply.github.com>
2023-01-20 01:00:21 +0530

    Update mdps_public1.py

--- mdps_public1.py
+++ mdps_public1.py
@@ -141,19 +141,15 @@ if (selected == 'Diabetes Prediction'):
     if st.button('Diabetes Test Result'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
-        if (diab_prediction[0] == 1):
-            
+        if (diab_prediction[0] == 1): 
           diab_diagnosis = 'The person is diabetic'
           warn_diabetic = 'There is a high probability that the person has Diabetes , It is Recommended to visit a Specialist Doctor as soon as possible Here is a list of Some Doctors that are available.'
-            
         else:
-            
           diab_diagnosis = 'The person is not diabetic'
-          warn_diabetic = 'To avoid diabetes follow the precautions given'
     
-        
-    st.success(diab_diagnosis)
-    st.warning(warn_diabetic)
+        st.success(diab_diagnosis)
+        st.warning(warn_diabetic)
+    
 
 
 
